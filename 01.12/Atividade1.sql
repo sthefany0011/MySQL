2fd72b69639241a183c7fcc69e0c7fd4ee4e4dc9
Sthefany Silva <sthefany0011@gmail.com>
2020-12-01 20:27:03 -0300

    first commit

---
+++ 01.12/Atividade1.sql
@@ -0,0 +1,15 @@
+id,nome,idade,departamento,ativo,salario
+1,"Joana Maria",34,Administrativo,1,3500
+2,"Guilherme de oliveira",30,Administrativo,1,3000
+4,"Pedro Athur",19,Administrativo,1,1500
+5,"Maria Souza",32,Administrativo,1,3500
+6,"Gabriela de Jesus",32,Financeiro,1,5500
+7,"Gabriela de Jesus",32,Financeiro,1,5500
+8,"Gabriel Salles",31,Financeiro,1,3500
+9,"Gustavo Silva",23,Financeiro,1,1500
+10,"Julia De Andrade",23,Financeiro,1,2000
+11,"Juliano De Andrade",29,Marketing,1,3500
+12,"Benedita Salmão",20,Marketing,1,4100
+13,"Giselle Augusta",18,Marketing,1,1800
+14,"Jose de Almeida",54,Marketing,1,5400
+15,"Julio Paz",38,Marketing,1,3800
